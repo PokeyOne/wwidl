fdee91dea711c9e796f74b38f6d32d5c9025848f
Mateo Carreras <mateo.carreras@gmail.com>
2022-01-05 14:29:01 -0800

    test for last message

--- src/config/tests.rs
+++ src/config/tests.rs
@@ -1,5 +1,27 @@
 use super::*;
 
+/// Simple macro to create an example config with two messages.
+macro_rules! create_example_config {
+    () => {
+        {
+            let str_data = "[[repos]]
+path = \"/path/to/repo\"
+
+[[repos.messages]]
+message = \"test message\"
+date = 2022-01-05T14:00:52.000000-08:00
+
+[[repos.messages]]
+message = \"test message 2\"
+date = 2022-01-05T14:00:53.000000-08:00\n";
+            println!("{}", str_data);
+            let config: Config = toml::from_str(str_data).unwrap();
+
+            config
+        }
+    }
+}
+
 #[test]
 fn default_config_should_have_not_repos() {
     let config = Config::default();
@@ -17,6 +39,21 @@ fn test_load_from_path() {
     let expected_date_string = "2022-01-05T14:00:52-08:00";
     assert_eq!(
         config.repos[0].messages()[0].date().to_string(),
+        expected_date_string
+    );
+}
+
+#[test]
+fn test_get_last_message() {
+    let config = create_example_config!();
+    let last_message = config.repo_data(&"/path/to/repo")
+        .unwrap()
+        .last_message()
+        .unwrap();
+    assert_eq!(last_message.message(), "test message 2");
+    let expected_date_string = "2022-01-05T14:00:53-08:00";
+    assert_eq!(
+        last_message.date().to_string(),
         expected_date_string
     );
 }
