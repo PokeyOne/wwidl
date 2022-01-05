f79649a1f56874e11443639f1b91fedad8455442
Mateo Carreras <mateo.carreras@gmail.com>
2022-01-04 17:07:40 -0800

    figure out a basic config file path'

--- src/config.rs
+++ src/config.rs
@@ -11,13 +11,30 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn load() -> Result<Config, ConfigLoadError> {
-        let os = match env::var("OS") {
-            Ok(os) => os,
-            Err(_) => "unknown".to_string(),
+    pub fn default() -> Self {
+        Config { }
+    }
+
+    pub fn load() -> Result<Self, ConfigLoadError> {
+        let system = env::consts::OS;
+        println!("System: {}", system);
+        if system == "windows" {
+            // TODO: !MC - Figure out where to store configs on windows
+            println!("Windows Operating System is not fully supported yet. Loading default config.");
+            return Ok(Self::default())
+        }
+
+        let home_dir = match env::var("HOME") {
+            Ok(path) => path,
+            Err(_) => {
+                println!("Could not find home directory. Ensure HOME env variable is set. Loading default config.");
+                return Ok(Self::default());
+            }
         };
 
-        println!("OS: {}", os);
+        let config_dir = home_dir + "/.wwidl/config.toml";
+        println!("Loading config from: {}", config_dir);
+
         todo!()
     }
 }
